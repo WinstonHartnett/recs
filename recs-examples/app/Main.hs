34649b64068630408d691faa1747f595e6585a13
Winston Hartnett <winstonhartnett@gmail.com>
2022-05-20 02:11:08 -0500

    Benchmarked eff state

--- recs-examples/app/Main.hs
+++ recs-examples/app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Main where
 
 import Recs.Lib
@@ -12,6 +14,11 @@ import Linear.Matrix
 import Data.Vector.Unboxed.Deriving ( derivingUnbox )
 import GHC.Generics (Generic)
 import Data.Word
+import Effectful
+import Effectful.State.Static.Local
+import Data.Coerce (coerce)
+import Criterion.Main
+import Control.DeepSeq (NFData)
 
 newtype Velocity = MkVelocity (V3 Float)
 
@@ -59,40 +66,119 @@ mySpawnF = spawn undefined do
 --   tagged $ MkPosition emptyV3
 --   tagged $ MkTransform emptyM44
 
-myFunc :: Ecs ()
-myFunc = do
-  -- createArch []
-  -- ecs <- get
-  -- let spawnSys :: Commands -> System ()
-  --     spawnSys c = do
-  --       VU.forM_ [0..100_000] (\(_ :: Word32) -> void $ spawn c 
-  --         >>= tagged (MkRotation emptyV3) 
-  --         >>= tagged (MkVelocity emptyV3)
-  --         >>= tagged (MkPosition emptyV3) 
-  --         >>= tagged (MkTransform emptyM44)
-  --         >>= finish)
-  --     posSys :: Query (Nab Position) -> System ()
-  --     posSys q = forQ q $ \qh -> do
-  --       p <- nab @Position qh
-  --       liftIO . print $ p
-  -- (_, (MkSystemState commands)) <- queried spawnSys >>= runSystem
-  -- commitCommands (fromJust commands) >> queried posSys >>= runSystem
---   let posSys (q :: Query (Nab Position |&| Nab Velocity)) = forQ q $ \qh -> do
---         p <- nab @Position qh
---         v <- nab @Velocity qh
---         liftIO . putStrLn $ show p <> " " <> show v
---       mySys :: Query (Nab Position |&| Nab Velocity) -> System ()
---       mySys q = forQ q $ \qh -> do
---         p <- nab @Position qh
---         liftIO . print $ p
---       myMaker c =
---         VU.forM_ [0..1000] (\p -> void $ spawn c >>= tagged (MkPosition p) >>= tagged (MkVelocity p) >>= finish)
---   (_, (MkSystemState commands)) <- queried myMaker >>= runSystem
-  -- commitCommands (fromJust commands) >> queried posSys >>= runSystem
-  pure ()
+newtype A = MkA {unA :: Int }
+  deriving (Generic, NFData)
+newtype B = MkB { unB :: Int}
+  deriving (Generic, NFData)
+newtype C = MkC Int
+  deriving (Generic, NFData)
+newtype D = MkD Int
+  deriving (Generic, NFData)
+newtype E = MkE Int
+  deriving (Generic, NFData)
+
+data Combined = MkCombined
+      { af :: !A
+      , bf :: !B
+      , cf :: !C
+      , df :: !D
+      , ef :: !E
+      }
+    deriving (Generic, NFData)
+
+modifyAll :: State Combined :> es => Eff es ()
+modifyAll = do
+  modify \c ->
+    MkCombined
+      { af = MkA $ (+1) (unA $ af c)
+      , bf = MkB $ (+1) (unB $ bf c)
+      , cf = coerce @(Int -> Int) (+1) (cf c)
+      , df = coerce @(Int -> Int) (+1) (df c)
+      , ef = coerce @(Int -> Int) (+1) (ef c)
+      }
+
+modA :: State A :> es => Eff es ()
+modA = modify \(MkA a) -> MkA (a + 1)
+
+modB :: State B :> es => Eff es ()
+modB = modify \(MkB a) -> MkB (a + 1)
+
+modC :: State C :> es => Eff es ()
+modC = modify \(MkC a) -> MkC (a + 1)
+
+modD :: State D :> es => Eff es ()
+modD = modify \(MkD a) -> MkD (a + 1)
+
+modE :: State E :> es => Eff es ()
+modE = modify \(MkE a) -> MkE (a + 1)
+
+modInd :: (State A :> es, State B :> es, State C :> es, State D :> es, State E :> es) => Eff es ()
+modInd = modA >> modB >> modC >> modD >> modE
+
+-- myFunc :: Ecs ()
+-- myFunc = do
+--   -- createArch []
+--   -- ecs <- get
+--   -- let spawnSys :: Commands -> System ()
+--   --     spawnSys c = do
+--   --       VU.forM_ [0..100_000] (\(_ :: Word32) -> void $ spawn c 
+--   --         >>= tagged (MkRotation emptyV3) 
+--   --         >>= tagged (MkVelocity emptyV3)
+--   --         >>= tagged (MkPosition emptyV3) 
+--   --         >>= tagged (MkTransform emptyM44)
+--   --         >>= finish)
+--   --     posSys :: Query (Nab Position) -> System ()
+--   --     posSys q = forQ q $ \qh -> do
+--   --       p <- nab @Position qh
+--   --       liftIO . print $ p
+--   -- (_, (MkSystemState commands)) <- queried spawnSys >>= runSystem
+--   -- commitCommands (fromJust commands) >> queried posSys >>= runSystem
+-- --   let posSys (q :: Query (Nab Position |&| Nab Velocity)) = forQ q $ \qh -> do
+-- --         p <- nab @Position qh
+-- --         v <- nab @Velocity qh
+-- --         liftIO . putStrLn $ show p <> " " <> show v
+-- --       mySys :: Query (Nab Position |&| Nab Velocity) -> System ()
+-- --       mySys q = forQ q $ \qh -> do
+-- --         p <- nab @Position qh
+-- --         liftIO . print $ p
+-- --       myMaker c =
+-- --         VU.forM_ [0..1000] (\p -> void $ spawn c >>= tagged (MkPosition p) >>= tagged (MkVelocity p) >>= finish)
+-- --   (_, (MkSystemState commands)) <- queried myMaker >>= runSystem
+--   -- commitCommands (fromJust commands) >> queried posSys >>= runSystem
+--   pure ()
 
 main :: IO ()
-main = pure ()
+main = defaultMain [
+  bench "modifyAll" $ nf (runPureEff . runState (MkCombined (MkA 0) (MkB 1) (MkC 2) (MkD 3) (MkE 4))) (replicateM_ 1000 modifyAll),
+  bench "modifyInd" $ nf (runPureEff . runState (MkA 0) . runState (MkB 0) . runState (MkC 0) . runState (MkD 0) . runState (MkE 0)) (replicateM_ 1000 modInd)
+  ]
 
--- main :: IO ()
--- main = void $ runStateT (unEcs myFunc) =<< def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
